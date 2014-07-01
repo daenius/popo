b63dab46494e958ceb64e6b8a72b55ea299b42f4
Daenius <zhu.dennis@gmail.com>
2014-07-01 00:57:46 -0700

    Generate Database as part of build
    * Updated db creation scripts to work with Gradle
    * Added Gradle tasks to run database scripts
    * Added Gradle tasks to copy database into assets
    * Added Gradle dependency on assemble tasks to copy db

--- utils/create_tables.py
+++ utils/create_tables.py
@@ -1,17 +1,23 @@
 __author__ = 'dennizhu'
 import sqlite3
+import os
+import inspect
+from os.path import join
 
-db_connect = sqlite3.connect('content.db')
+current_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+db_connect = sqlite3.connect(join(current_path, 'content.db'))
 c = db_connect.cursor()
 
 # Create table
 c.execute(
-    "CREATE TABLE IF NOT EXISTS poi(_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+    "CREATE TABLE poi(_id INTEGER PRIMARY KEY AUTOINCREMENT, "
     "name TEXT, type INTEGER, latitude REAL, longitude REAL, school_id INTEGER, contenttext TEXT, "
     "picture BLOB, lastupdated INTEGER, notes TEXT, "
     "FOREIGN KEY(school_id) REFERENCES schools(_id))")
 c.execute(
-    "CREATE TABLE IF NOT EXISTS schools(_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+    "CREATE TABLE schools(_id INTEGER PRIMARY KEY AUTOINCREMENT, "
     "name TEXT, centerLatitude REAL, centerLongitude REAL)")
+c.close()
 db_connect.commit()
 db_connect.close()
+print "Tables Created Successfully"
--- utils/populate_tables.py
+++ utils/populate_tables.py
@@ -2,19 +2,24 @@ __author__ = 'dennizhu'
 import sqlite3
 import csv
 
-db_connect = sqlite3.connect('content.db')
+import os
+import inspect
+from os.path import join
+
+current_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+db_connect = sqlite3.connect(join(current_path, 'content.db'))
 db_connect.row_factory = sqlite3.Row
 
 csv.register_dialect('popo', delimiter='|', quoting=csv.QUOTE_MINIMAL)
 
-with open('popo_schools.csv', 'rb') as f:
+with open(join(current_path, 'popo_schools.csv'), 'rb') as f:
     reader = csv.DictReader(f, dialect='popo')
     to_db = [(row['_id'], row['name'], row['centerLatitude'], row['centerLongitude']) for row in reader]
 
 cursor = db_connect.cursor()
 cursor.executemany("INSERT INTO schools (_id, name, centerLatitude, centerLongitude) VALUES (?, ?, ?, ?);", to_db)
 
-with open('popo.csv', 'rb') as f:
+with open(join(current_path, 'popo.csv'), 'rb') as f:
     reader = csv.DictReader(f, dialect='popo')
     to_db = [(row['name'], row['type'], row['latitude'], row['longitude'], row['school_id'], row['contenttext'],
               row['lastupdated'], row['notes']) for row in reader]
@@ -28,3 +33,4 @@ db_connect.commit()
 
 cursor.close()
 db_connect.close()
+print "Tables Populated Successfully"
